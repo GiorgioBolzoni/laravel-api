22283a879b0a740829649d2b9d215d49a0aaccfc
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-01-19 14:49:47 +0100

    aggiunto Api/ProjectController e modificato

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $projects
+            ]
+        );
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $project
+            ]
+        );
+    }
+}
